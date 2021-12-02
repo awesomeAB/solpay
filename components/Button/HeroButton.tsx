68db6226d77a114dbd6c7b0f45d815c03908002a
ninjasushi4 <abhi@staderlabs.com>
2021-12-02 22:42:54 +0000

    add email input

--- components/Button/HeroButton.tsx
+++ components/Button/HeroButton.tsx
@@ -10,12 +10,11 @@ const Button: FC<Props> = ({ label, onClick }) => {
     <div className="relative group">
       <div className="absolute opacity-80 inset-0.5 bg-gradient-to-r from-solanaGreen to-purple-600 rounded-xl filter blur-lg group-hover:opacity-100 transition duration-200" />
       <button
-        className="relative flex items-center px-12 py-4 leading-none focus:outline-none bg-snow dark:bg-dark rounded-xl"
+        className="relative flex items-center px-12 py-3 leading-none focus:outline-none bg-snow dark:bg-dark rounded-2xl"
         onClick={onClick}
       >
         <i className="pr-6 text-3xl text-solanaGreen ri-wallet-3-line" />
-        <span className="pr-6 text-xl leading-6">{label}</span>
-        <span className="text-3xl">&rarr;</span>
+        <span className="pr-6 font-bold leading-6">{label}</span>
       </button>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,20 @@ import type { NextPage } from "next";
 import { useState } from "react";
 
 const Home: NextPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email);
+    setSubmitting(true);
+    setTimeout(() => {
+      setSubmitting(false);
+      setSuccess(true);
+    }, 1000);
+  };
 
   return (
     <div className="flex justify-center h-screen dark:bg-dark">
@@ -26,7 +39,36 @@ const Home: NextPage = () => {
             It'll only take two minutes to set up.
           </Text>
         </section>
-        <HeroButton label="Live Demo" onClick={() => setIsOpen(true)} />
+        <div className="flex flex-wrap items-center justify-center">
+          <HeroButton label="Live Demo" onClick={() => setIsOpen(true)} />
+          <form className="relative mx-8 my-12 group" onSubmit={onSubmit}>
+            <div className="flex items-center h-16 w-72 p-0.5 bg-gradient-to-tr from-solanaGreen to-purple-600 rounded-2xl">
+              <input
+                type="email"
+                placeholder="Enter Waitlist"
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                className="w-full h-full p-4 leading-none text-dark dark:text-snow focus:outline-none dark:bg-dark rounded-2xl"
+              />
+              <button
+                className="flex items-center h-full px-4 text-2xl text-snow"
+                disabled={success}
+                type="submit"
+              >
+                {submitting ? (
+                  <div className="flex items-center justify-center px-0.5">
+                    <div className=" w-5 h-5 border-t-2 border-b-2 rounded-full border-snow animate-spin"></div>
+                  </div>
+                ) : success ? (
+                  <i className="ri-check-double-line"></i>
+                ) : (
+                  <i className="ri-arrow-right-line"></i>
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
         <WalletConnectModal isOpen={isOpen} setIsOpen={setIsOpen} />
       </main>
     </div>
