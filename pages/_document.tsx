acc435683e53be5ff2b2c92d08ebc25a3c16a62e
ninjasushi4 <abhi@staderlabs.com>
2021-12-03 00:09:14 +0000

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,38 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+const GA_MEASUREMENT_ID = "G-LZJE6PMRL7";
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet" />
-          <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
+            rel="stylesheet"
+          />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
