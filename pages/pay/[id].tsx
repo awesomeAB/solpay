a045e6a4df0f8db3a7f135315fb8e60638f9afeb
sagred <sagred17@gmail.com>
2022-03-18 02:27:22 +0530

    added error check

--- pages/pay/[id].tsx
+++ pages/pay/[id].tsx
@@ -10,19 +10,30 @@ const Pay = () => {
 
   const [url, setUrl] = useState<string>("");
   const [isUrlFetched, setIsUrlFetched] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getIndividualPayment(id);
-      if (data) setUrl(data[0].url);
-      setIsUrlFetched(true);
+      if (data.length > 0) {
+        setUrl(data[0]?.url);
+        setIsUrlFetched(true);
+      } else {
+        setError("Invalid URL");
+      }
     };
     fetchData();
   }, [id]);
 
   return (
     <div className="flex items-center justify-center pt-32">
-      {isUrlFetched ? <PayPage url={url} /> : <LoadingDots />}
+      {isUrlFetched ? (
+        <PayPage url={url} />
+      ) : error ? (
+        <span className="text-lg text-red-600">Invalid reference</span>
+      ) : (
+        <LoadingDots />
+      )}
     </div>
   );
 };
